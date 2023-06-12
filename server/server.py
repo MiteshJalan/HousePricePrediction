fc73b0af249933da795a9aa1bd94e480d53cbff5
Mitesh Jalan <47392781+MiteshJalan@users.noreply.github.com>
2023-06-12 02:23:34 -0400

    Dir Name Update

--- server/server.py
+++ server/server.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import util
 
 app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('..\client\app.html') 
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
