b7afc72140da95f8bd4950be366d6182353ab114
Mitesh Jalan <47392781+MiteshJalan@users.noreply.github.com>
2023-06-17 22:16:18 -0400

    login page

--- server/server.py
+++ server/server.py
@@ -1,7 +1,31 @@
 from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import util
 # import sys
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+# from flask_bcrypt import Bcrypt
+ 
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisissecret'
+db = SQLAlchemy(app)
+
+class User(db.Model, UserMixin):
+    id= db.Column(db.Integer, primary_key=True)
+    username= db.Column(db.String(20), nullable=False, unique=True)
+    password= db.Column(db.String(80), nullable=True)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Register')
+
 
 @app.route('/')
 def home():
