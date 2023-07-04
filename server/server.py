855c32331125aecfd53b7ef3880d44ccdddb2d00
Mitesh Jalan <47392781+MiteshJalan@users.noreply.github.com>
2023-07-04 00:41:19 -0400

    user status

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, redirect,url_for, session
+from flask import Flask, request, jsonify, render_template, redirect,url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import util
@@ -6,22 +6,36 @@ import sys
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, StringField, TextAreaField
+from wtforms.validators import InputRequired, Length, ValidationError, DataRequired
 from flask_bcrypt import Bcrypt
- 
+# from flask.cli import FlaskGroup
+from flask_migrate import Migrate
+from hashlib import md5 #for avatar
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisissecret'
+# app.cli.add_command(MigrateCommand)
+# cli = FlaskGroup(app)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 db = SQLAlchemy(app)
 bcrypt=Bcrypt(app)
-
+migrate = Migrate(app, db)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @login_manager.user_loader
@@ -34,13 +48,39 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=True)
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def avatar(self, size):
+        digest = md5(self.username.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
 
 #flask form to register new user and verify if user already exist
 class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
     password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField('Register')
-
     def validate_username(self, username):
         existing_user_username = User.query.filter_by(username=username.data).first()
         if existing_user_username:
@@ -71,6 +111,32 @@ def login():
                 return redirect(url_for('home'))
     return render_template('login.html', form=form)
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('_edit_profile.html', title='Edit Profile',
+                           form=form)
+
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
     return render_template('dashboard.html')
@@ -91,7 +157,7 @@ def signup():
         new_user = User(username=form.username.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('login'))
+        return render_template(url_for('login'))
     return render_template('signup.html', form=form)
 
 @app.route('/get_location_names', methods=['GET'])
@@ -126,4 +192,6 @@ if __name__ == "__main__":
     #     print(f"Error occurred during table creation: {e}")
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run(host='0.0.0.0')
+    db.init_app(app)
+    migrate.init_app(app, db)
+    app.run(debug=True,host='0.0.0.0')
--- server/static/js/app.js
+++ server/static/js/app.js
@@ -29,8 +29,8 @@ function onClickedEstimatePrice() {
   
   // Construct the URL based on the host
   // var url = "http://" + host + "/get_location_names";
-  // var url = "http://127.0.0.1:5000/predict_home_price"; 
-  var url = "https://house-price-banglore-portal.onrender.com/predict_home_price"; 
+  var url = "http://127.0.0.1:5000/predict_home_price"; 
+  // var url = "https://house-price-banglore-portal.onrender.com/predict_home_price"; 
   //Use this if you are NOT using nginx 
   // var url = "/api/predict_home_price"; // Use this if using nginx.
   $.post(url, {
@@ -52,8 +52,8 @@ function onPageLoad() {
   
   // Construct the URL based on the host
   // var url = "http://" + host + "/get_location_names";
-  // var url = "http://127.0.0.1:5000/get_location_names"
-  var url = "https://house-price-banglore-portal.onrender.com/predict_home_price"; 
+  var url = "http://127.0.0.1:5000/get_location_names"
+  // var url = "https://house-price-banglore-portal.onrender.com/predict_home_price"; 
   // var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
   $.get(url,function(data, status) {
       console.log("got response for get_location_names request");
