f7450fdc8ca3a4459c8aaea8460e02d0715259da
Mitesh Jalan <47392781+MiteshJalan@users.noreply.github.com>
2023-07-31 16:18:26 -0400

    chatroom

--- server/server.py
+++ server/server.py
@@ -11,6 +11,27 @@ from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
  
 
+<<<<<<< Updated upstream
+=======
+from flask_migrate import Migrate
+from hashlib import md5 #for avatar
+
+from flask_mail import Mail, Message #for password reset
+from time import time
+import jwt
+from threading import Thread
+
+from flask_socketio import join_room, leave_room, send, SocketIO
+from string import ascii_uppercase
+import os
+import random
+
+###################################################################
+# Config
+###################################################################
+
+#-------------------Basic Config-----------------------------------
+>>>>>>> Stashed changes
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisissecret'
@@ -18,6 +39,14 @@ app.config['SECRET_KEY'] = 'thisissecret'
 db = SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 
+<<<<<<< Updated upstream
+=======
+socketio = SocketIO(app)
+
+
+
+#_________________________User Auth________________________________
+>>>>>>> Stashed changes
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -34,6 +63,135 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=True)
+<<<<<<< Updated upstream
+=======
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+
+    def avatar(self, size):
+        digest = md5(self.username.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+    
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+    
+    
+    # keep track of followers
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    #JWT for reseting password
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+    
+
+
+
+
+
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+#Followers:
+
+
+
+# ________________________________________________________________
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[Length(min=5, max=20),DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
+
+# ----------------------Reset Password -------------------
+
+# class ResetPasswordRequestForm(FlaskForm):
+#     email = StringField('Email', validators=[DataRequired(), Email()])
+#     submit = SubmitField('Request Password Reset')
+
+# class ResetPasswordForm(FlaskForm):
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     password2 = PasswordField(
+#         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+#     submit = SubmitField('Request Password Reset')
+
+###################################################################
+# Forms
+###################################################################
+>>>>>>> Stashed changes
 
 #flask form to register new user and verify if user already exist
 class RegisterForm(FlaskForm):
@@ -51,6 +209,7 @@ class LoginForm(FlaskForm):
     password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField('Login')
 
+<<<<<<< Updated upstream
 
 @app.route('/home')
 @login_required
@@ -58,6 +217,12 @@ def home():
     if "user" in session:
         user = session["user"]
         return render_template("app.html", username=user)
+=======
+###################################################################
+# Routes
+###################################################################
+
+>>>>>>> Stashed changes
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -96,9 +261,7 @@ def signup():
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
-    response = jsonify({
-        'locations': util.get_location_names()
-    })
+    response = jsonify({'locations': util.get_location_names()})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
@@ -110,11 +273,307 @@ def predict_home_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
-    })
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+<<<<<<< Updated upstream
+=======
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    form = EmptyForm()
+    return render_template('user.html', user=user, posts=posts.items,next_url=next_url, prev_url=prev_url, form=form)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user', username=current_user.username))
+    
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+
+    return render_template('_edit_profile.html',title='Edit Profile',form=form)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+#-----------------User Post----------------------
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+# ______________________Follow/ Unfollow___________
+
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are following {}!'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash('You are not following {}.'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/index', methods=['GET', 'POST'])
+@login_required
+def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form,
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
+
+@app.route('/explore')
+@login_required
+def explore():
+    # posts = Post.query.order_by(Post.timestamp.desc()).all()
+    # return render_template('index.html', title='Explore', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template("index.html", title='Explore', posts=posts.items,
+                          next_url=next_url, prev_url=prev_url)
+
+#---------------------------error handeling-----------------#
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html')
+
+
+
+# _____________________email______________________________________
+
+
+# def send_email(subject, sender, recipients, text_body, html_body):
+#     msg = Message(subject, sender=sender, recipients=recipients)
+#     msg.body = text_body
+#     msg.html = html_body
+#     mail.send(msg)
+
+# def send_password_reset_email(user):
+#     token = user.get_reset_password_token()
+#     send_email('[Microblog] Reset Your Password',
+#                sender=app.config['ADMINS'][0],
+#                recipients=[user.email],
+#                text_body=render_template('email/reset_password.txt',
+#                                          user=user, token=token),
+#                html_body=render_template('email/reset_password.html',
+#                                          user=user, token=token))
+
+# def send_password_reset_email(user):
+#     token = user.get_reset_password_token()
+#     send_email('[HousePricePrediction] Reset Your Password',
+#                sender=app.config['ADMINS'][0],
+#                recipients=[user.email],
+#                text_body=render_template('email/reset_password.txt',
+#                                          user=user, token=token),
+#                html_body=render_template('email/reset_password.html',
+#                                          user=user, token=token))
+
+# def send_async_email(app, msg):
+#     with app.app_context():
+#         mail.send(msg)
+
+
+# def send_email(subject, sender, recipients, text_body, html_body):
+#     msg = Message(subject, sender=sender, recipients=recipients)
+#     msg.body = text_body
+#     msg.html = html_body
+#     Thread(target=send_async_email, args=(app, msg)).start()
+
+
+
+
+
+# @app.route('/reset_password_request', methods=['GET', 'POST'])
+# def reset_password_request():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = ResetPasswordRequestForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(email=form.email.data).first()
+#         if user:
+#             send_password_reset_email(user)
+#         flash('Check your email for the instructions to reset your password')
+#         return redirect(url_for('login'))
+#     return render_template('reset_password_request.html',
+#                            title='Reset Password', form=form)
+
+# @app.route('/reset_password/<token>', methods=['GET', 'POST'])
+# def reset_password(token):
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     user = User.verify_reset_password_token(token)
+#     if not user:
+#         return redirect(url_for('index'))
+#     form = ResetPasswordForm()
+#     if form.validate_on_submit():
+#         user.set_password(form.password.data)
+#         db.session.commit()
+#         flash('Your password has been reset.')
+#         return redirect(url_for('login'))
+#     return render_template('reset_password.html', form=form)
+
+
+
+
+# ______________________________ChatRoom___________________
+rooms = {}
+
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+        if code not in rooms:
+            break
+    return code
+
+@app.route("/joinroom", methods=["POST", "GET"])
+def joinroom():
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+        if not name:
+            return render_template('joinroom.html', error="Please enter a name.", code=code, name=name)
+        if join != False and not code:
+            return render_template('joinroom.html', error="Please enter a room code.", code=code, name=name)
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages": []}
+        elif code not in rooms:
+            return render_template('joinroom.html', error="Room does not exist.", code=code, name=name)
+        session["room"] = room
+        session["name"] = name
+        return redirect(url_for("room"))
+    return render_template('joinroom.html')
+
+@app.route("/room")
+def room():
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("joinroom"))
+    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return 
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f"{name} has left the room {room}")
+
+'''
+###################################################################
+Main
+###################################################################
+'''
+>>>>>>> Stashed changes
 
 if __name__ == "__main__":
     # try:
@@ -126,4 +585,11 @@ if __name__ == "__main__":
     #     print(f"Error occurred during table creation: {e}")
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
+<<<<<<< Updated upstream
     app.run(host='0.0.0.0')
+=======
+    db.init_app(app)
+    migrate.init_app(app, db)
+    # app.run(debug=True,host='0.0.0.0')
+    socketio.run(app, debug=True, host='0.0.0.0')
+>>>>>>> Stashed changes
