ea10458d0bc72b1cd676e1f210b9de308172f093
Mitesh Jalan <47392781+MiteshJalan@users.noreply.github.com>
2023-07-31 16:47:22 -0400

    chatroom

--- server/server.py
+++ server/server.py
@@ -17,7 +17,9 @@ from flask_mail import Mail, Message #for password reset
 from time import time
 import jwt
 from threading import Thread
-
+from flask_socketio import join_room, leave_room, send, SocketIO
+import random
+from string import ascii_uppercase
 
 import os
 
@@ -36,13 +38,12 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 db = SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 migrate = Migrate(app, db)
-<<<<<<< Updated upstream
-=======
+
 
 socketio = SocketIO(app)
 
 
->>>>>>> Stashed changes
+
 
 #_________________________User Auth________________________________
 
@@ -528,6 +529,86 @@ def internal_error(error):
 #         return redirect(url_for('login'))
 #     return render_template('reset_password.html', form=form)
 
+#_______________________________Chatroom____________________
+rooms = {}
+
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+        if code not in rooms:
+            break
+    return code
+
+@app.route("/joinroom", methods=["POST", "GET"])
+def joinroom():
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+        if not name:
+            return render_template('joinroom.html', error="Please enter a name.", code=code, name=name)
+        if join != False and not code:
+            return render_template('joinroom.html', error="Please enter a room code.", code=code, name=name)
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages": []}
+        elif code not in rooms:
+            return render_template('joinroom.html', error="Room does not exist.", code=code, name=name)
+        session["room"] = room
+        session["name"] = name
+        return redirect(url_for("room"))
+    return render_template('joinroom.html')
+
+@app.route("/room")
+def room():
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("joinroom"))
+    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return 
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f"{name} has left the room {room}")
+
 
 
 
@@ -549,9 +630,6 @@ if __name__ == "__main__":
     util.load_saved_artifacts()
     db.init_app(app)
     migrate.init_app(app, db)
-<<<<<<< Updated upstream
     app.run(debug=True,host='0.0.0.0')
-=======
     # app.run(debug=True,host='0.0.0.0')
     socketio.run(app, debug=True, host='0.0.0.0')
->>>>>>> Stashed changes
